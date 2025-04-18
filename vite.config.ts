beaab34cf70a4cb850a2cf30db7f8f02e7fc4520
alimuthana222 <153876752+alimuthana222@users.noreply.github.com>
2025-04-18 16:18:05 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
@@ -19,4 +20,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
+                           export default defineConfig({
+  plugins: [react()],
+  base: "/student-exchange/", 
+});
+);
